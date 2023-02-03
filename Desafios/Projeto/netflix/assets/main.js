7871b9bd31ea71ed9278a35b9aefd1a8ecad78b5
Anthony Samuel Freitas <84025181+sammyfreitas@users.noreply.github.com>
2023-02-03 19:18:37 -0300

    Add files via upload

---
+++ Desafios/Projeto/netflix/assets/main.js
@@ -0,0 +1,33 @@
+// let aElement = document.getElementsByClassName('aElement');
+//     aElement[0].addEventListener('mouseover', overMouse);
+//     aElement[1].addEventListener('mouseover', overMouse);
+//     aElement[2].addEventListener('mouseover', overMouse);
+//     aElement[0].addEventListener('mouseout', normalize);
+//     aElement[1].addEventListener('mouseout', normalize);
+//     aElement[2].addEventListener('mouseout', normalize);
+//     var varover;
+
+
+
+// window.addEventListener('scroll', function () {
+// 	let header = document.querySelector('.allMenu');
+// 	header.classList.toggle('allMenuActive', window.scrollY > 0);
+// })
+
+// function overMouse (over) {
+//     varover = over.toElement.parentElement 
+//     if (varover.tagName == 'LI') {
+//         let imgOver = varover.firstElementChild;
+//         imgOver.style.border = '4px solid white';
+//         imgOver.style.boxSizing = 'border-box'
+//         let spanOver = varover.lastElementChild;
+//         spanOver.style.color = 'white';
+//     }
+// }
+
+// function normalize (over) {
+//     let imgOver = varover.firstElementChild;
+//     imgOver.style.border = 'none';
+//     let spanOver = varover.lastElementChild;
+//     spanOver.style.color = 'grey';
+// }
